06da3e2dc242957bd691acc07a78c19cbfff893e
AndriiShainoha <55914660+AndriiShainoha@users.noreply.github.com>
2020-04-20 14:32:27 +0300

    Added changes with apple

---
+++ SnakeLab/Entities/Food/AppleWithJuice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLab.Entities.Food
+{
+    class AppleWithJuice : FoodDecorator
+    {
+        public AppleWithJuice(GameEntity gameEntity)
+        {
+
+        }
+    }
+}
---
+++ SnakeLab/Entities/Food/FoodDecorator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLab.Entities.Food
+{
+    abstract class FoodDecorator : GameEntity
+    {
+        protected GameEntity gameEntity;
+        public FoodDecorator(GameEntity gameEntity)
+        {
+            this.gameEntity = gameEntity;
+        }
+    }
+}
